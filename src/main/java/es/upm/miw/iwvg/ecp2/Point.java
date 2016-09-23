f1bd3ebcbb672c65cafa1d86c3686f3622a43ff5
JRSP <JRSP@Javi-AsusA555L>
2016-09-23 19:56:25 +0200

    Create setX and setY in class point

--- src/main/java/es/upm/miw/iwvg/ecp2/Point.java
+++ src/main/java/es/upm/miw/iwvg/ecp2/Point.java
@@ -37,7 +37,15 @@ public class Point {
     public int getY() {
         return this.y;
     }
+    
+    public void setX(int x) {
+        this.x=x;
+    }
 
+    public void setY(int y) {
+    	 this.y=y;
+    }
+    
     @Override
     public String toString() {
         return "Point[" + x + "," + y + "]";
